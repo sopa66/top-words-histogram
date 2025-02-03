11fee0d4e62015ea845a70a631f556aeea64a2f4
sopa66 <stpnstpnvc@gmail.com>
2025-02-03 16:58:02 +0400

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[]) {
-    QGuiApplication app(argc, argv);
+    const QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     QObject::connect(
